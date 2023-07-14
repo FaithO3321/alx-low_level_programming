5184908e189d783cacf26aa61eb9251fd9aaaf5f
FaithO3321 <faithomondi1331@gmail.com>
2023-07-14 14:49:02 -0700

    Program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Prints any commbination of numbers with commas
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
