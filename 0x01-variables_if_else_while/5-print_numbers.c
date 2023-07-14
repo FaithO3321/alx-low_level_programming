6c94402549aa0ceadc65285f478cc1d811da2f61
FaithO3321 <faithomondi1331@gmail.com>
2023-07-14 13:50:08 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Prints numbers to base 10
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
