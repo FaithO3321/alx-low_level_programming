7f8c71462844c44b3596f583f20a9b3952f11ae6
FaithO3321 <faithomondi1331@gmail.com>
2023-07-20 09:25:41 -0700

    Sum of multiple numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: A program that prints the sum of multiples of 3 and 5
+ * Return: 0
+*/
+int main(void)
+{
+	int sum3 = 0, sum5 = 0, sum23 = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum3 += i;
+		}
+		if (i % 5 == 0)
+		{
+			sum5 += i;
+		}
+		if (i % 23 == 0)
+		{
+			sum23 += i;
+		}
+	}
+	printf("%d\n", sum3 + sum5 - sum23);
+	return (0);
+}
+
