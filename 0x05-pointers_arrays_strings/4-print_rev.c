e97f2e4e336a970b2cb9463c8373511a167db4af
FaithO3321 <faithomondi1331@gmail.com>
2023-07-25 11:20:37 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,9 +5,15 @@
  */
 void print_rev(char *s)
 {
-	while (*s)
+	int length = 0;
+
+	while (s[length] != '\0')
 	{
-		_putchar(*s--);
+		length++;
+	}
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
