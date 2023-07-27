fa639e728ac702ee056d18204f956e218a62735a
FaithO3321 <faithomondi1331@gmail.com>
2023-07-27 16:22:36 -0700

    Capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: Input string
+ * Return: Pointer to the resulting string
+*/
+char *cap_string(char *str)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	char *ptr = str;
+	int i, capitalize_next = 1;
+
+	while (*ptr != '\0')
+	{
+		if (capitalize_next && *ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		for (i = 0; separators[i] != '\0'; i++)
+		{
+			if (*ptr == separators[i])
+			{
+			capitalize_next = 1;
+			break;
+			}
+			else
+			{
+			capitalize_next = 0;
+			}
+		}
+	}
+	{
+	ptr++;
+	}
+	return (str);
+}
