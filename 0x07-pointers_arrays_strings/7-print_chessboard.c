128a50002381fefdb4357e4c8e111502f1d08b5a
FaithO3321 <faithomondi1331@gmail.com>
2023-08-01 17:57:59 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - Prints chessboard
+ * Return: 0
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	char chessboard[8][8] = {
+		{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+	};
+
+	print_chessboard(chessboard);
+
+	return (0);
+}
