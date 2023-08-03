8b472b456387442f2c3a2a1ded071760648697eb
FaithO3321 <faithomondi1331@gmail.com>
2023-08-03 14:38:27 -0700

    Palindrome headache

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,57 @@
 #include "main.h"
-#include <string.h>
-/**
- * is_palindrome - checks if a string is palindrome
- * @s: the string to check
- * Return: 1 if string is palindrome,
- * 0 otherwise
-*/
-int is_palindrome(char *s)
-{
-	int length = strlen(s);
 
-	if (length <= 1)
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - returns the length of a string
+ * @s: string to be measured
+ * Return: string length
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - checks if string is a palindrome.
+ * @s: string to be checked.
+ * @len: string length
+ * @index: string index to be checked
+ * Return: 1 if the string is a palindrome, otherwise 0
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
 		return (1);
 
-	if (s[0] != s[length - 1])
-		return (0);
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
 
-	s[length - 1] = '\0';
+	return (0);
+}
 
-	return (is_palindrome(s + 1));
+/**
+ * is_palindrome - checks if a string is a palindrome.
+ * @s: string to be checked.
+ * Return: 1 if the string is a palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
 }
