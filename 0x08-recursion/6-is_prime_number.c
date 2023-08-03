df2e3be3d99ed1956638ddfcf8421ab032f670fb
FaithO3321 <faithomondi1331@gmail.com>
2023-08-02 19:24:24 -0700

    Return prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: the number
+ * Return: 1 if the input integer is a prime number,
+ * otherwise return 0
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (is_prime_number(n));
+	return (is_prime_number(n - 1));
+}
