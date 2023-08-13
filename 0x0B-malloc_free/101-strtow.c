e104d8e305d0b0a4c62f7c149aa9054c98b7dcb4
FaithO3321 <faithomondi1331@gmail.com>
2023-08-13 12:17:40 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,39 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * strtow - splits a string into words
- * @str: the string to split
- * Return: pointer to an array of strings (words),
- * or NULL if str is NULL or empty
+ * strtow -  splits a string into two words
+ * @str: the string to be split
+ * Return: a pointer to an array of strings (words) or NULL,
+ * if string is NULL or str is empty
  */
 char **strtow(char *str)
 {
-	int wordcount = 0, inword = 0, i = 0;
-	char **words = NULL, *wordstart = NULL, *p = str;
+	char **words, *temp;
+	int i, j, k, word_count = 0, len = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (; *p != '\0'; p++)
-		if (*p == ' ' && inword && (inword = 0))
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			word_count++;
-		else if (*p != ' ' && !in_word && (in_word = 1))
-			word_start = p;
-	if (in_word)
-		word_count++;
-
-	words = malloc(sizeof(char *) * (word_count + 1));
+	}
+	words = malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
-	for (p = str; *p != '\0'; p++)
-		if (*p == ' ' && in_word && (in_word = 0))
-			*p = '\0', words[i++] = word_start;
-		else if (*p != ' ' && !in_word && (in_word = 1))
-			word_start = p;
-	if (in_word)
-		words[i++] = word_start;
-		words[i] = NULL;
-
+	i = 0;
+	while (*str != '\0')
+	{
+		while (*str == ' ')
+			str++;
+		temp = str;
+		len = 0;
+		while (*str != ' ' && *str != '\0')
+		{
+			len++;
+			str++;
+		}
+		words[i] = malloc((len + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			words[i][k] = temp[k];
+		words[i][k] = '\0';
+		i++;
+	}
+	words[i] = NULL;
 	return (words);
 }
