220ad5fa951b678bdbfc95d84d1caab528ec4469
FaithO3321 <faithomondi1331@gmail.com>
2023-08-14 15:18:13 -0700

    Reallocate mem

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,14 +15,27 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	new_ptr = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	new_ptr	= malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
+
+	if (new_size < old_size)
+		old_size = new_size;
 
 	memcpy(new_ptr, ptr, old_size);
 
 	free(ptr);
-
 	return (new_ptr);
 }
