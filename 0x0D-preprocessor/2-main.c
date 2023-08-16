460dbfc2b58daa09395d4f8911f49c6cedc4e82e
FaithO3321 <faithomondi1331@gmail.com>
2023-08-15 18:20:06 -0700

    name of compiled file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: 0
+*/
+int main(void)
+{
+	char * __PREVIOUS__;
+
+	printf("Name of the file compiled from: %s\n", __PREVIOUS__);
+		return (0);
+}
