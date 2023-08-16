be3afe4a9ca6c31ef940f4d7d854c779806c9838
FaithO3321 <faithomondi1331@gmail.com>
2023-08-15 19:51:53 -0700

    macrof

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef MACROS_ABS_H
+#define MACROS_ABS_H
+#include <stdio.h>
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif /* MACROS_ABS_H */
---
+++ 0x0D-preprocessor/macroabs.h
@@ -0,0 +1,7 @@
+#ifndef MACROABS_H
+#define MACROABS_H
+#include <stdio.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* MACROABS_H */
