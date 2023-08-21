c80cc10504430d36cf3e65dd03314a44dfb37adf
FaithO3321 <faithomondi1331@gmail.com>
2023-08-21 04:00:37 -0700

    new dog name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,11 @@ typedef struct dog
 	char *name;
 	char *owner;
 	float age;
-} dog_t
-
+} dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
