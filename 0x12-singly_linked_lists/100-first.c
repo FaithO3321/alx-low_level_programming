5caef3b67c5e41282212dda281dc830dc70e6505
FaithO3321 <faithomondi1331@gmail.com>
2023-08-28 02:52:27 -0700

    BEfore main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - prints message before calling ain function
+*/
+void before_main(void) __attribute__ ((constructor));
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
