58dcb1141fb0bf638fbc037ce75b3ab610cf821e
FaithO3321 <faithomondi1331@gmail.com>
2023-09-17 16:43:54 -0700

    Copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - entry point
+ * @argc: arguemnt count
+ * @argv: argument vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int from_fd, to_fd, read_bytes;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		dprintf(2, "Usage: cp file_from file_to\n");
+			exit(97);
+
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+
+	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR |
+			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (to_fd == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+
+	while ((read_bytes = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(to_fd, buffer, read_bytes) != read_bytes)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+		}
+	}
+	if (read_bytes == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+
+	if (close(from_fd) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", from_fd);
+			exit(100);
+
+	if (close(to_fd) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", to_fd);
+			exit(100);
+
+	exit(0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,9 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
 
+int main(int argc, char *argv[]);
 int _strlen(char *s);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
